3a8c55817dbe490147619b08c76fc83a6d090ad1
Zachary Viggiani <zach@bigmojo.net>
2017-05-06 15:07:49 -0400

    7 done

--- ajax.js
+++ ajax.js
@@ -79,4 +79,50 @@ $(document).ready(function () {
 
     console.log('After .ajax');
   });
+
+  /* Number 6 */
+
+  $('#step8').on('click', function() {
+    console.log('Before .ajax');
+
+    $.ajax({
+      url:'http://first-ajax-api.herokuapp.com/ping',
+      method: 'GET',
+      dataType: 'text',
+
+    }).done(function(data) {
+      $('#step8').append(data);
+      console.log('Got: ' + data)
+    }).fail(function(jqXHR, textStatus) {
+      console.log('.ajax FAILED with ' + jqXHR.status + ' ' + jqXHR.statusText);
+      console.log(jqXHR);
+    }).always(function() {
+      console.log('.ajax request finished');
+    });
+
+    console.log('After .ajax');
+  });
+
+  /* Number  */
+
+  $('#step9').on('click', function() {
+    console.log('Before .ajax');
+
+    $.ajax({
+      url:'http://first-ajax-api.herokuapp.com/count',
+      method: 'GET',
+      dataType: 'text',
+
+    }).done(function(data) {
+      $('#step9').append(data);
+      console.log('Got: ' + data)
+    }).fail(function(jqXHR, textStatus) {
+      console.log('.ajax FAILED with ' + jqXHR.status + ' ' + jqXHR.statusText);
+      console.log(jqXHR);
+    }).always(function() {
+      console.log('.ajax request finished');
+    });
+
+    console.log('After .ajax');
+  });
 });
