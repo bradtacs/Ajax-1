b93ac92d2915892b23e9e67ad4616abfe2104644
Zachary Viggiani <zach@bigmojo.net>
2017-05-06 14:53:50 -0400

    4 done

--- ajax.js
+++ ajax.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
 
-    /* Number 1 and two */
+  /* Number 1 and two */
+
   $('#step12').on('click', function() {
     console.log('Before .ajax');
 
@@ -11,27 +12,50 @@ $(document).ready(function () {
     });
   });
   console.log('After .ajax');
-    /* Number 3 */
 
-    $('#step3456').on('click', function() {
-      console.log('Before .ajax');
+  /* Number 3 */
 
-      $.ajax({
-        url:'http://first-ajax-api.herokuapp.com/ping',
-        method: 'GET',
-        dataType: 'text',
+  $('#step3456').on('click', function() {
+    console.log('Before .ajax');
 
-      }).done(function(data) {
-        $('#step3456').append(data);
-        console.log('Got: ' + data)
-      }).fail(function(jqXHR, textStatus) {
-        console.log('.ajax FAILED with ' + jqXHR.status + ' ' + jqXHR.statusText);
-        console.log(jqXHR);
-      }).always(function() {
-        console.log('.ajax request finished');
-      });
+    $.ajax({
+      url:'http://first-ajax-api.herokuapp.com/ping',
+      method: 'GET',
+      dataType: 'text',
 
-        console.log('After .ajax');
+    }).done(function(data) {
+      $('#step3456').append(data);
+      console.log('Got: ' + data)
+    }).fail(function(jqXHR, textStatus) {
+      console.log('.ajax FAILED with ' + jqXHR.status + ' ' + jqXHR.statusText);
+      console.log(jqXHR);
+    }).always(function() {
+      console.log('.ajax request finished');
     });
+
+    console.log('After .ajax');
+  });
+  /* Number 3 */
+
+  $('#step7').on('click', function() {
+    console.log('Before .ajax');
+
+    $.ajax({
+      url:'http://first-ajax-api.herokuapp.com/pong',
+      method: 'GET',
+      dataType: 'text',
+
+    }).done(function(data) {
+      $('#step7').append(data);
+      console.log('Got: ' + data)
+    }).fail(function(jqXHR, textStatus) {
+      console.log('.ajax FAILED with ' + jqXHR.status + ' ' + jqXHR.statusText);
+      console.log(jqXHR);
+    }).always(function() {
+      console.log('.ajax request finished');
+    });
+
+    console.log('After .ajax');
+  });
 
 });
