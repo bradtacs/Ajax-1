b9b3b4232ca72343ea884f80ae9942d972147daf
Zachary Viggiani <zach@bigmojo.net>
2017-05-06 13:44:17 -0400

    1 and two donr

--- ajax.js
+++ ajax.js
@@ -1,5 +1,14 @@
 $(document).ready(function () {
 
-  /* Your code goes here */
+    /* Your code goes here */
+  $('#step12').on('click', function() {
+    console.log('Before .ajax');
 
+    $.ajax({
+      url:'http://first-ajax-api.herokuapp.com/',
+      method: 'GET',
+      dataType: 'text',
+    });
+  });
+  console.log('After .ajax');
 });
